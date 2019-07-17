3bd47596b64a3b8bcd3ade58b5e5c02b875dd746
AlexShukhman <alex.jacob.shukhman@gmail.com>
2019-07-16 20:35:21 -0400

    update imports

--- terrabl.py
+++ terrabl.py
@@ -26,10 +26,10 @@ Main            -- Main class and runner
 
 # --- Imports ---
 # Local
-from .parser.parser import Parser
-from .installer.installer import Installer
-from .client.crawler.crawler import Crawler
-from .client.file_manager.file_manager import File_Manager
+from parser.parser import Parser
+from installer.installer import Installer
+from client.crawler.crawler import Crawler
+from client.file_manager.file_manager import File_Manager
 
 # Imported
 
